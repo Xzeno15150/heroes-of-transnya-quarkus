6d402b89dd342219b8db9c4985dd2cf8536c617a
Erina Point <erina.point@etu.uca.fr>
2023-05-26 11:41:14 +0200

    Ensemble des classes metiers

--- src/main/java/fr/multiplatform/hot/entities/Statistic.java
+++ src/main/java/fr/multiplatform/hot/entities/Statistic.java
@@ -3,4 +3,25 @@ package fr.multiplatform.hot.entities;
 public class Statistic {
     private int strength;
     private int intelligence;
+
+    public Statistic(int strength, int intelligence) {
+        this.strength = strength;
+        this.intelligence = intelligence;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
 }
