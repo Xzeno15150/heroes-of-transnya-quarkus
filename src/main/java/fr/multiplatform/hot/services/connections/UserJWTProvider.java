4ba31d1810eaf1a67f42f5dbf36d2409f67ba6a5
Erina Point <erina.point@etu.uca.fr>
2023-06-07 13:56:23 +0200

    jwt ca marche

--- src/main/java/fr/multiplatform/hot/services/connections/UserJWTProvider.java
+++ src/main/java/fr/multiplatform/hot/services/connections/UserJWTProvider.java
@@ -1,5 +1,7 @@
 package fr.multiplatform.hot.services.connections;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import fr.multiplatform.hot.entities.Role;
 import fr.multiplatform.hot.entities.user.User;
 import fr.multiplatform.hot.exceptions.UserNotFoundException;
@@ -7,6 +9,9 @@ import fr.multiplatform.hot.resources.dtos.ConnectionUserDTO;
 import io.smallrye.jwt.build.Jwt;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,22 +21,38 @@ public abstract class UserJWTProvider {
     @ConfigProperty(name="jwt.issuer")
     private String issuer;
 
+    @ConfigProperty(name = "jwt.secret")
+    private String jwtSecret;
+
+    @ConfigProperty(name = "jwt.ttl")
+    private int jwtTTL;
+
+
     public String getUserJWT(ConnectionUserDTO userDTO) {
         User user = getUser(userDTO);
         if (user == null) {
             throw new UserNotFoundException("Mot de passe ou login invalid");
         }
 
-        Set<String> roles = user.getRoles().stream()
+        List<String> roles = user.getRoles().stream()
                 .map(Role::toString)
-                .collect(Collectors.toSet());
+                .collect(Collectors.toList());
 
-        return Jwt.issuer(issuer)
-                .groups(roles)
-                .claim("email", user.getEmail())
-                .claim("username", user.getUsername())
-                .sign();
+        return this.createToken(user.getEmail(), user.getUsername(), roles);
     }
 
+
+    public String createToken(String email, String username, List<String> roles) {
+        Algorithm algorithm = Algorithm.HMAC256(jwtSecret);
+        Instant now = Instant.now();
+        return JWT.create()
+                .withClaim("email", email)
+                .withClaim("username", username)
+                .withClaim("groups", roles)
+                .withIssuer(issuer)
+                .withIssuedAt(now)
+                .withExpiresAt(now.plus(jwtTTL, ChronoUnit.SECONDS))
+                .sign(algorithm);
+    }
     protected abstract User getUser(ConnectionUserDTO userDTO);
 }
